77940c279e51b8ea027f4303df92bfa11ecb89b8
NihhZaeyy <nizaralmas11@gmail.com>
2023-09-06 23:55:33 +0700

    adding and routing Food Page

---
+++ src/ThingToDoComponents/Foods/Components/FoodButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FoodButton = () => {
+  return (
+    <div>
+        <a href="" className='bg-button flex w-[120px]  justify-center mx-auto py-2 px-3  text-white rounded-2xl my-3 font-Poppins'>Read More</a>
+    </div>
+  )
+}
+
+export default FoodButton
---
+++ src/ThingToDoComponents/Foods/Components/FoodCard.jsx
@@ -0,0 +1,33 @@
+import React , { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+import FoodButton from './FoodButton'
+import { ReactComponent as Love } from '../../Museum/Assets/icon-love.svg'
+
+
+const FoodCard = ({item , src , title , description}) => {
+    
+    const [toggle, setToggle] = useState(false)
+    const clickLove = () => {
+        setToggle(!toggle)
+    }
+  return (
+    <div className='w-full mb-5 lg:w-[25%] md:w-[30%] md:p-2'>
+            <div className="relative">
+                <img src={src} alt="" className='w-full h-60 rounded-lg mb-2 object-cover' />
+                <div onClick={clickLove} className='absolute bottom-0 right-0 bg-[#F1F8FF] w-[40px] h-[40px] opacity-90  rounded-br-md rounded-tl-md hover:bg-button hover:fill-white cursor-pointer'>
+                    <Love  className={`p-2 ${toggle ?  'bg-button fill-white rounded-br-md rounded-tl-md hover:bg-white hover:fill-button'  : 'fill-button hover:fill-white '}`}/>
+                </div>
+            </div>
+            <div className='h-[90px]'>
+                <h3>{title}</h3>
+                <p className='text-[14px] line-clamp-3'>{description}</p>
+            </div>
+            <Link to={`/things-to-do/food-jogja/${item}`}> 
+                <FoodButton />
+            </Link>
+    </div>
+  )
+}
+
+export default FoodCard
