f513ff54664de2896ed24755c902e3fd1b522a07
nadila98dev <nadilarizkyputri@gmail.com>
2023-09-09 21:48:19 +0700

    add souvenir API

---
+++ src/API/apiSouvenir.js
@@ -0,0 +1,12 @@
+const API_SOUVENIR = "ttps://my-json-server.typicode.com/nadila98dev /apiSouvenir"
+
+import axios from "axios"
+import React from "react"
+
+const axiosSouvenir = {
+    souvenir: async() => await axios.get(API_SOUVENIR +'/souvenir').then((res) => res.data).catch((err) => { console.log(err)}),
+    detail: async (id) => await axios.get(`${API_SOUVENIR}/souvenir/${id}`).then((res) => res.data).catch((err) => { console.log(err)}),
+
+}
+
+export default axiosSouvenir
