9bb30b22b64f21b0b4a1f1617deeaf95e0deb474
NihhZaeyy <nizaralmas11@gmail.com>
2023-09-11 05:46:44 +0700

    add div for generating saved page list

---
+++ src/Components/Atoms/ActivatedButtonLove.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ActivatedButtonLove = () => {
+  return (
+    <div>
+        <button className='px-3 py-2 xl:px-4 xl:py-2 bg-button2 rounded-tl-lg rounded-br-lg text-[22px] xl:text-[32px]'>
+            <i className='bx bxs-heart text-button  font-semibold'></i>
+        </button>
+    </div>
+  )
+}
+
+export default ActivatedButtonLove
