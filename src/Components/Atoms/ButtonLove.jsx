eecbd83970f9ebb510ac72e688418ae2cad97e16
NihhZaeyy <nizaralmas11@gmail.com>
2023-09-11 06:11:10 +0700

    fixing 3# styles on carousel main-page and fixing button menu

--- src/Components/Atoms/ButtonLove.jsx
+++ src/Components/Atoms/ButtonLove.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ButtonLove = () => {
   return (
     <div>
-        <button className='px-3 py-2 xl:px-4 xl:py-2 bg-button2 rounded-tl-lg rounded-br-lg xl:text-[32px]'>
+        <button className='px-3 py-2 xl:px-4 xl:py-2 bg-button2 rounded-tl-lg rounded-br-lg text-[24px] xl:text-[32px]'>
             <i className='bx bx-heart text-button'></i>
         </button>
     </div>
