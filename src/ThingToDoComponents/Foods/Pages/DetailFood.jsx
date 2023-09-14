959deb844c8cbf0ba3efaa44d8d6f4184ebc23af
komporbakar <laravelgg00@gmail.com>
2023-09-15 03:00:05 +0700

    optimize calling API & update search page

--- src/ThingToDoComponents/Foods/Pages/DetailFood.jsx
+++ src/ThingToDoComponents/Foods/Pages/DetailFood.jsx
@@ -1,84 +1,97 @@
-import React , {useState, useEffect} from 'react'
-import ReuseNav from '../../../Components/ReuseableNav/ReuseNav'
-import Footer from '../../../Components/Footer/Footer'
-import BotButton from '../../../Components/Landing/Landing-bot/BotButton';
+import React, { useState, useEffect } from "react";
+import ReuseNav from "../../../Components/ReuseableNav/ReuseNav";
+import Footer from "../../../Components/Footer/Footer";
+import BotButton from "../../../Components/Landing/Landing-bot/BotButton";
 
-import { useNavigate } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-import axiosFood from '../../../API/apiFood';
+import { useNavigate } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import axiosFood from "../../../API/apiFood";
+import axiosInstance from "../../../API/apiCall";
 
 const DetailFood = () => {
-
   // Route
-  const navigateToFoodPage = useNavigate ()
+  const navigateToFoodPage = useNavigate();
 
-  const [detail, setDetail] = useState({})
-  const {id} = useParams()
+  const [detail, setDetail] = useState({});
+  const { id } = useParams();
 
-  const callApi = async()=>{
-    axiosFood.detail(id).then((res) =>{
-      setDetail(res)
-    })
-  }
-
+  const callApi = async () => {
+    axiosInstance.detail(id).then((res) => {
+      const data = res[0];
+      setDetail(data);
+    });
+  };
   useEffect(() => {
-    callApi()
-  }, [])
-    return (
-        <div className="flex flex-col min-h-screen pb-5">
-            <ReuseNav />
-            <div className='w-screen h-[80px] flex justify-end items-center pr-4 gap-3
+    callApi();
+  }, []);
+  return (
+    <div className="flex flex-col min-h-screen pb-5">
+      <ReuseNav />
+      <div
+        className="w-screen h-[80px] flex justify-end items-center pr-4 gap-3
                 sm:pr-5
-            '>
-                <div className='flex gap-3 items-center bg-button2 rounded-xl w-[270px] cursor-pointer drop-shadow-md bottom-14'
-                    onClick={() => navigateToFoodPage('/things-to-do/food-jogja')}>
-                    <button 
-                        id='botButton' 
-                        className='text-button  text-3xl px-[6px] py-[1px] w-[3rem] h-[3rem] flex justify-center items-center z-[1] '
-                        >
-                        <i className='bx bx-x'></i>
-                    </button>
-                    <p className='font-Poppins'>Back to Food Page</p>
-                </div>
-            </div>
-            <div className='lg:flex lg:items-center lg:justify-center mb-4'>
-                <img src={detail.pic} alt="" className='w-full lg:min-h-[96vh] object-cover rounded-2xl drop-shadow-xl'/>
-            </div>
-            <div className='w-screen h-fit bg-[#F1F8FF] pt-5'>
-                <section className="px-5 md:w-[75%] md:mx-auto ">
-                    <div className="flex flex-1  justify-between items-center mb-5">
-                        <div>
-                            <span className='font-Poppins'>Food</span>
-                        </div>
-                        <button className='bg-button2 w-[100px] flex items-center px-4 py-2 rounded-xl'>
-                            <p className='text-button me-1 font-Poppins'>Share </p>
-                            <i className='bx bx-share-alt text-[20px] text-button'></i>
-                        </button>
-                        <button className='flex items-center bg-button2 w-[100px] px-4 py-2 rounded-xl'>
-                            <p className='text-button me-1 font-Poppins'>Save </p>
-                            <i className='bx bx-heart text-[20px] text-button'></i>
-                        </button>
-                    </div>
-                    
-                    <div>
-                        <h1 className="mb-5 font-bold text-4xl font-Lora">{detail.title}</h1>
-                        <span className='mb-5 text-[16px] font-semibold'><span>Rp.</span> {detail.price}</span>
-                        <p className="mt-2 text-[18px] text-justify"> {detail.description}</p>
-                            <div className='flex justify-center items-center my-5 '>
-                                <button className='bg-button w-[160px] flex items-center px-4 py-2 rounded-xl'>
-                                    <p className='text-white me-1 font-Poppins'>Order Food </p>
-                                    <i className='bx bx-cart-alt text-[22px] text-white ml-1'></i>
-                                </button>
-                            </div>
-                    </div>
-                </section>
-            </div>
-            <div className='flex justify-center items-center'>
-                <BotButton />
-            </div>
-            <Footer />
+            "
+      >
+        <div
+          className="flex gap-3 items-center bg-button2 rounded-xl w-[270px] cursor-pointer drop-shadow-md bottom-14"
+          onClick={() => navigateToFoodPage("/things-to-do/food-jogja")}
+        >
+          <button
+            id="botButton"
+            className="text-button  text-3xl px-[6px] py-[1px] w-[3rem] h-[3rem] flex justify-center items-center z-[1] "
+          >
+            <i className="bx bx-x"></i>
+          </button>
+          <p className="font-Poppins">Back to Food Page</p>
         </div>
-      )
-}
+      </div>
+      <div className="lg:flex lg:items-center lg:justify-center mb-4">
+        <img
+          src={detail.src}
+          alt=""
+          className="w-full lg:min-h-[96vh] object-cover rounded-2xl drop-shadow-xl"
+        />
+      </div>
+      <div className="w-screen h-fit bg-[#F1F8FF] pt-5">
+        <section className="px-5 md:w-[75%] md:mx-auto ">
+          <div className="flex flex-1  justify-between items-center mb-5">
+            <div>
+              <span className="font-Poppins">Food</span>
+            </div>
+            <button className="bg-button2 w-[100px] flex items-center px-4 py-2 rounded-xl">
+              <p className="text-button me-1 font-Poppins">Share </p>
+              <i className="bx bx-share-alt text-[20px] text-button"></i>
+            </button>
+            <button className="flex items-center bg-button2 w-[100px] px-4 py-2 rounded-xl">
+              <p className="text-button me-1 font-Poppins">Save </p>
+              <i className="bx bx-heart text-[20px] text-button"></i>
+            </button>
+          </div>
 
-export default DetailFood
+          <div>
+            <h1 className="mb-5 font-bold text-4xl font-Lora">{detail.name}</h1>
+            <span className="mb-5 text-[16px] font-semibold">
+              <span>Rp.</span> {detail.price}
+            </span>
+            <p className="mt-2 text-[18px] text-justify">
+              {" "}
+              {detail.description}
+            </p>
+            <div className="flex justify-center items-center my-5 ">
+              <button className="bg-button w-[160px] flex items-center px-4 py-2 rounded-xl">
+                <p className="text-white me-1 font-Poppins">Order Food </p>
+                <i className="bx bx-cart-alt text-[22px] text-white ml-1"></i>
+              </button>
+            </div>
+          </div>
+        </section>
+      </div>
+      <div className="flex justify-center items-center">
+        <BotButton />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default DetailFood;
--- src/ThingToDoComponents/Foods/Pages/Food.jsx
+++ src/ThingToDoComponents/Foods/Pages/Food.jsx
@@ -1,51 +1,84 @@
-import React from 'react'
-import Navbar from '../../../Components/Landing/Navbar/Navbar'
-import Footer from '../../../Components/Footer/Footer'
+import React, { useEffect, useState } from "react";
+import Navbar from "../../../Components/Landing/Navbar/Navbar";
+import Footer from "../../../Components/Footer/Footer";
 
-import FoodCard from '../Components/FoodCard'
-import FoodsData from '/src/Database/foodsData.json'
+import FoodCard from "../Components/FoodCard";
+import axiosInstance from "../../../API/apiCall";
+// import FoodsData from "/src/Database/foodsData.json";
 
 const Food = () => {
+  const [foods, setFoods] = useState([]);
+  const callApi = async () => {
+    axiosInstance.items(4).then((res) => {
+      console.log(res);
+      setFoods(res);
+    });
+  };
+  useEffect(() => {
+    callApi();
+  }, []);
   return (
-    <div className='flex flex-col min-h-screen '>
-        <div className='w-screen h-screen flex flex-col items-center bg-[url("/Assets/foods/background-food-page.png")] bg-cover bg-no-repeat bg-center bg-fixed'>
-            <Navbar />
-            <div className='w-screen sm:w-[70vw] h-[90vh] flex justify-center items-center px-10 pb-[7rem] flex-col'>
-                <h1 className='text-white text-5xl lg:text-[72px] italic text-center'>Foods</h1>
-                <p className='text-white text-[12px] text-center mt-5 lg:w-[50vw] lg:text-[16px]'>The food in Jogja is a tantalizing blend of rich flavors and unique ingredients that create a culinary experience like no other.</p>
-            </div>
+    <div className="flex flex-col min-h-screen ">
+      <div className='w-screen h-screen flex flex-col items-center bg-[url("/Assets/foods/background-food-page.png")] bg-cover bg-no-repeat bg-center bg-fixed'>
+        <Navbar />
+        <div className="w-screen sm:w-[70vw] h-[90vh] flex justify-center items-center px-10 pb-[7rem] flex-col">
+          <h1 className="text-white text-5xl lg:text-[72px] italic text-center">
+            Foods
+          </h1>
+          <p className="text-white text-[12px] text-center mt-5 lg:w-[50vw] lg:text-[16px]">
+            The food in Jogja is a tantalizing blend of rich flavors and unique
+            ingredients that create a culinary experience like no other.
+          </p>
         </div>
-        <div className='bg-white shadow-lg border-spacing-3 px-5 '>
-            <div className='flex flex-col md:flex-row md:px-[40px] md:justify-between lg:px-[80px] xl:px-[140px]'>
-                <div className='flex flex-col justify-center md:items-start w-full py-8'>
-                    <h1 className=' mt-5 mb-4 text-4xl font-medium tracking-tight leading-none md:text-5x1 xl:text-6x1  font-lora'>Foods</h1> 
-                        <div className="flex ">
-                            Explore foods
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 ml-2">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
-                                </svg>
-                        </div> 
-                </div>
-                <div className='flex w-full md:w-[400px] lg:w-[500px] xl:w-[600px]  text-[12px] lg:text-[16px] md:items-center md:text-start lg:pt-6'>
-                    <p>The food in Jogja is simply extraordinary, boasting a delightful blend of flavors that will tantalize your taste buds and leave you craving for more.</p>
-                </div>
+      </div>
+      <div className="bg-white shadow-lg border-spacing-3 px-5 ">
+        <div className="flex flex-col md:flex-row md:px-[40px] md:justify-between lg:px-[80px] xl:px-[140px]">
+          <div className="flex flex-col justify-center md:items-start w-full py-8">
+            <h1 className=" mt-5 mb-4 text-4xl font-medium tracking-tight leading-none md:text-5x1 xl:text-6x1  font-lora">
+              Foods
+            </h1>
+            <div className="flex ">
+              Explore foods
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6 ml-2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+                />
+              </svg>
             </div>
-            <section className='flex flex-wrap justify-center mt-10 gap-5'>
-                {FoodsData.map((food , index) => (
-                    <FoodCard 
-                        key={index}
-                        item={food.id}
-                        title={food.title}
-                        src={food.pic}
-                        description={food.description}
-                    />
-                ))}
-            </section>
+          </div>
+          <div className="flex w-full md:w-[400px] lg:w-[500px] xl:w-[600px]  text-[12px] lg:text-[16px] md:items-center md:text-start lg:pt-6">
+            <p>
+              The food in Jogja is simply extraordinary, boasting a delightful
+              blend of flavors that will tantalize your taste buds and leave you
+              craving for more.
+            </p>
+          </div>
         </div>
-    
-        <Footer />
-    </div>
-  )
-}
+        <section className="flex flex-wrap justify-center mt-10 gap-5">
+          {foods.map((food, index) => (
+            <FoodCard
+              key={index}
+              item={food.slug}
+              title={food.name}
+              src={food.src}
+              description={food.description}
+            />
+          ))}
+        </section>
+      </div>
 
-export default Food
+      <Footer />
+    </div>
+  );
+};
+
+export default Food;
